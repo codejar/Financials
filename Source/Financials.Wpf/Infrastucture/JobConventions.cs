709270b1b0e9c6ba0ffab2c3891fbd074718a15b
Roland Pheasant <roland_pheasant@hotmail.com>
2015-02-18 11:02:52 +0000

    Use structure map conventions for registration

---
+++ Source/Financials.Wpf/Infrastucture/JobConventions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using StructureMap.Graph;
+using StructureMap.TypeRules;
+using StructureMap.Configuration.DSL;
+
+namespace Financials.Wpf.Infrastucture
+{
+    /// <summary>
+    /// A class ending in 'Job' is always registered as a singleton
+    /// </summary>
+    internal class JobConventions : IRegistrationConvention
+    {
+        public void Process(Type type, Registry registry)
+        {
+            if (type.IsConcrete() && type.Name.EndsWith("Job"))
+                registry.For(type).Singleton();
+        }
+    }
+}
